7763ac9b3cdbaed2f2f026d3d2e1eaafba0f252d
wjdgnsdl213 <wjdgnsdl213@gmail.com>
2024-02-16 02:09:20 +0900

    bypass-protected email

--- phishing_crawler.py
+++ phishing_crawler.py
@@ -4,35 +4,34 @@ from urllib.parse import urljoin
 
 # 크롤링할 대상 사이트 URL
 base_url = 'https://phishtank.org/'
-search_url = 'phish_search.php?page={}&valid=y&Search=Search'
+search_url = 'phish_search.php?page={}&valid=y&active=y&Search=Search'
 
-# 각 게시물 페이지에서 실제 피싱 링크를 추출하는 함수
-
+# Cloudflare 인코딩 된 이메일 디코딩하는 함수
+def decode_cf_email(encoded_string):
+    r = int(encoded_string[:2], 16)
+    email = ''.join([chr(int(encoded_string[i:i+2], 16) ^ r) for i in range(2, len(encoded_string), 2)])
+    return email
 
 def extract_link(post_url):
     response = requests.get(post_url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        padded_divs = soup.find_all('div', class_='padded')
+        b_element = soup.select_one('#widecol > div > div:nth-child(4) > span > b')
 
-        for padded_div in padded_divs:
-            inner_div = padded_div.find('div', style=None, class_=None)
-            if inner_div:
-                span_element = inner_div.find('span')
-                if span_element:
-                    b_element = span_element.find('b')
-                    if b_element:
-                        link_text = b_element.text.strip()
-                        # Cloudflare Email Protection 확인
-                        if '[email protected]' in link_text:
-                            return None  # 이메일 포함된 링크는 건너뜁니다
-                        else:
-                            return link_text
+        if b_element:
+            # Cloudflare로 보호된 이메일이 있는 경우 디코드
+            cf_email = b_element.find(class_='__cf_email__')
+            if cf_email and 'data-cfemail' in cf_email.attrs:
+                encoded_email = cf_email['data-cfemail']
+                decoded_email = decode_cf_email(encoded_email)
+                cf_email.replace_with(decoded_email)
+            link_text = b_element.get_text(strip=True)
+            return link_text
     return None
 
+
+
 # 페이지마다 게시물을 크롤링하는 함수
-
-
 def crawl_phish_tank(start_page, end_page):
     num = 0
     for page in range(start_page, end_page + 1):
@@ -49,16 +48,12 @@ def crawl_phish_tank(start_page, end_page):
 
                 if extracted_link:
                     num += 1
-                    print("------------")
-                    print(f"DATA NUM: {num}")
-                    print(f"Page: {page}")
-                    print(f"Post Link: {post_link}")
-                    print(f"Extracted Link: {extracted_link}")
-
+                    print(f"[+] DATA NUM {num}")
+                    print(f"    |-- Page: {page}")
+                    print(f"    |-- Post Link: {post_link}")
+                    print(f"    |-- Extracted Link: {extracted_link}")
         else:
-            print(
-                f"Error {response.status_code}: Failed to retrieve the webpage for page {page}.")
-
+            print(f"Error {response.status_code}: Failed to retrieve the webpage for page {page}.")
 
 # 크롤링 실행
-crawl_phish_tank(1, 1)
+crawl_phish_tank(8, 8)
--- phishing_crawler_makeCSV.py
+++ phishing_crawler_makeCSV.py
@@ -1,39 +1,37 @@
 import requests
-import csv
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import csv
 
 # 크롤링할 대상 사이트 URL
 base_url = 'https://phishtank.org/'
-search_url = 'phish_search.php?page={}&valid=y&Search=Search'
+search_url = 'phish_search.php?page={}&valid=y&active=y&Search=Search'
 
-# 각 게시물 페이지에서 실제 피싱 링크를 추출하는 함수
-
+# Cloudflare 인코딩 된 이메일 디코딩하는 함수
+def decode_cf_email(encoded_string):
+    r = int(encoded_string[:2], 16)
+    email = ''.join([chr(int(encoded_string[i:i+2], 16) ^ r) for i in range(2, len(encoded_string), 2)])
+    return email
 
 def extract_link(post_url):
     response = requests.get(post_url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        padded_divs = soup.find_all('div', class_='padded')
+        b_element = soup.select_one('#widecol > div > div:nth-child(4) > span > b')
 
-        for padded_div in padded_divs:
-            inner_div = padded_div.find('div', style=None, class_=None)
-            if inner_div:
-                span_element = inner_div.find('span')
-                if span_element:
-                    b_element = span_element.find('b')
-                    if b_element:
-                        link_text = b_element.text.strip()
-                        # Cloudflare Email Protection 확인
-                        if '[email protected]' in link_text:
-                            return None  # 이메일 포함된 링크는 건너뜁니다
-                        else:
-                            return link_text
+        if b_element:
+            # Cloudflare로 보호된 이메일이 있는 경우 디코드
+            cf_email = b_element.find(class_='__cf_email__')
+            if cf_email and 'data-cfemail' in cf_email.attrs:
+                encoded_email = cf_email['data-cfemail']
+                decoded_email = decode_cf_email(encoded_email)
+                cf_email.replace_with(decoded_email)
+            link_text = b_element.get_text(strip=True)
+            return link_text
     return None
 
+
 # 페이지마다 게시물을 크롤링하는 함수
-
-
 def crawl_phish_tank(start_page, end_page):
     num = 0
     extracted_links = []
@@ -52,28 +50,33 @@ def crawl_phish_tank(start_page, end_page):
 
                 if extracted_link:
                     extracted_links.append({
-                        'Extracted Link': extracted_link
+                        'url': extracted_link
                     })
-
         else:
             print(
                 f"Error {response.status_code}: Failed to retrieve the webpage for page {page}.")
 
     return extracted_links
 
-# CSV 파일로 저장하는 함수
 
-def save_to_csv(data, filename='extracted_links.csv'):
-    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+# CSV 파일로 저장하는 함수
+def save_to_csv(data, filename='extracted_links.csv', page=None):
+    mode = 'a' if page > 0 else 'w' 
+    header = page == 0
+    
+    with open(filename, mode, newline='', encoding='utf-8') as csvfile:
         fieldnames = ['Extracted Link']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-        writer.writeheader()
+        if header:
+            writer.writeheader()
         for item in data:
-            writer.writerow(item)
+            writer.writerow({'Extracted Link': item['url']})
 
 
-# 크롤링 실행 및 CSV 파일로 저장
-extracted_data = crawl_phish_tank(0, 0)
-save_to_csv(extracted_data, 'extracted_links.csv')
+for start_page in range(0, 25000):
+    print(f"start: {start_page}") 
+    extracted_data = crawl_phish_tank(start_page, start_page+1) 
+    save_to_csv(extracted_data, 'test.csv', start_page)
+
 print("추출된 링크가 extracted_links.csv 파일에 저장되었습니다.")
